bdbe8cc6223a89af3f92b54cc25f5b49e248e660
Diego F. Aranha <dfaranha@gmail.com>
2021-04-14 22:40:24 -0300

    montgomery_jump_alt_template.c

--- inversion-c/montgomery_test_template.c
+++ inversion-c/montgomery_test_template.c
@@ -21,7 +21,7 @@ inline long long cpucycles(void) {
 }
 
 long long t[64];
-WORD x[LIMBS];
+WORD x[SAT_LIMBS];
 
 void bench(void) {
   long long i,j;
@@ -59,6 +59,8 @@ void bench(void) {
   for (i = 1;i < 64;++i)
     printf(" %lld",t[i]);
   printf("\n");
+  printf("median");
+  printf(" %lld\n",t[32]);
   
   fflush(stdout);
 }
